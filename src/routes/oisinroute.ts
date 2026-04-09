84efd7703ab5b4d596ad9c443faaffa5fdaeab22
OisinPG <oisinpg@uw.edu>
2026-04-09 14:41:05 -0700

    My oisinroute.ts wasn't pretty enough to pass the lint test (changed to use single quotes)

--- src/routes/oisinroute.ts
+++ src/routes/oisinroute.ts
@@ -1,9 +1,9 @@
-import { Router } from "express";
-
+import { Router } from 'express';
 import { oisinHello } from '../controllers/oisincontroller';
-
+ 
 const oisinRouter = Router();
-
+ 
 oisinRouter.get('/hello/oisin', oisinHello);
-
+ 
 export { oisinRouter };
+ 
