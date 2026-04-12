096b6cfc109e25e695a057ea79fe45af4fc053b8
Icefire113 <phelan.gormley@gmail.com>
2026-04-12 15:45:05 -0700

    log if we fail to start the server

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,10 @@ import { app } from './app';
 
 const PORT = parseInt(process.env.PORT || '3000', 10);
 
-app.listen(PORT, () => {
+app.listen(PORT, (err: Error | undefined) => {
+  if (err) {
+    console.error(`Error starting server: ${err}`);
+  }
   console.log(`Server running at http://localhost:${PORT}`);
   console.log(`API docs at http://localhost:${PORT}/api-docs`);
 });
