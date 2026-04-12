38ee5b538a7b7299f1f73ffa1ca750709338d5e5
Icefire113 <phelan.gormley@gmail.com>
2026-04-12 15:43:58 -0700

    display different url depending on environment (prod or dev)

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,14 @@ import 'dotenv/config';
 import { app } from './app';
 
 const PORT = parseInt(process.env.PORT || '3000', 10);
+const isProd = process.env.NODE_ENV === 'production';
 
 app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
-  console.log(`API docs at http://localhost:${PORT}/api-docs`);
+  if (!isProd) {
+    console.log(`Server running at http://localhost:${PORT}`);
+    console.log(`API docs at http://localhost:${PORT}/api-docs`);
+  } else {
+    console.log(`Server running at https://group-project-backend-group-4.onrender.com`);
+    console.log(`API docs at https://group-project-backend-group-4.onrender.com/api-docs`);
+  }
 });
