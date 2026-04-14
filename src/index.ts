e1029f3e2ab025c25fb2d789c4be3fa4464e7e26
Icefire113 <phelan.gormley@gmail.com>
2026-04-14 14:10:38 -0700

    Update eslint for sprint 1

--- src/index.ts
+++ src/index.ts
@@ -6,14 +6,19 @@ const isProd = process.env.NODE_ENV === 'production';
 
 app.listen(PORT, (err: Error | undefined) => {
   if (err) {
+    // eslint-disable-next-line no-console
     console.error(`Error starting server: ${err}`);
     return;
   }
   if (!isProd) {
+    // eslint-disable-next-line no-console
     console.log(`Server running at http://localhost:${PORT}`);
+    // eslint-disable-next-line no-console
     console.log(`API docs at http://localhost:${PORT}/api-docs`);
   } else {
+    // eslint-disable-next-line no-console
     console.log(`Server running at https://group-project-backend-group-4.onrender.com`);
+    // eslint-disable-next-line no-console
     console.log(`API docs at https://group-project-backend-group-4.onrender.com/api-docs`);
   }
 });
