0801239c4d056802755d62ce6e5a22fe9a41d747
Icefire113 <phelan.gormley@gmail.com>
2026-04-21 12:34:34 -0700

    Remove 2x logger

--- src/app.ts
+++ src/app.ts
@@ -14,7 +14,6 @@ const app = express();
 app.use(logger);
 app.use(cors());
 app.use(express.json());
-app.use(logger);
 
 // OpenAPI documentation
 const specFile = fs.readFileSync('./openapi.yaml', 'utf8');
